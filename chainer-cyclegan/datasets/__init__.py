2984db3b6e89a176d257f450ce6cccc9ebe9f98e
Takahito Ogawa <takahito.ogawa@datagrid.co.jp>
2018-05-07 05:39:10 +0900

    add source code

---
+++ chainer-cyclegan/datasets/__init__.py
@@ -0,0 +1,5 @@
+#from .horse2zebra import horse2zebra_train
+#from .horse2zebra import horse2zebra_test
+#from .lsun_bedroom_line2color import lsun_bedroom_line2color_train
+from .silverhair import silverhair_train
+from .white_background import white_train
